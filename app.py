007a81b8dd363231ce19d9717dc2edef996b8da2
Yogesh Ingulkar <yogeshingulkar750@gmail.com>
2025-11-17 14:06:46 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+@app.route('/api/products', methods=['POST'])
+def create_product():
+    data = request.json
+    
+    # Create new product
+    product = Product(
+        name=data['name'],
+        sku=data['sku'],
+        price=data['price'],
+        warehouse_id=data['warehouse_id']
+    )
+    
+    db.session.add(product)
+    db.session.commit()
+    
+    # Update inventory count
+    inventory = Inventory(
+        product_id=product.id,
+        warehouse_id=data['warehouse_id'],
+        quantity=data['initial_quantity']
+    )
+    
+    db.session.add(inventory)
+    db.session.commit()
+    
+    return {"message": "Product created", "product_id": product.id}
